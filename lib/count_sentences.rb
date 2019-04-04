33f6278953d91cfdc225a49cd62e5d15503b244e
PYN <yxp7531540@gmail.com>
2019-04-03 22:15:00 -0500

    passed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
+    num_of_sentences = 0
 
+    self.split.each do |letter|
+      num_of_sentences += 1 if letter[-1] == "."|| letter[-1] == "?" || letter[-1] == "!"
+    end
+
+    num_of_sentences
   end
 end
